d64a4b03659a3951c3eb7e76b9b5e55797f7069e
dupontL64 <45233269+dupontL64@users.noreply.github.com>
2019-10-09 16:01:41 +0200

    ajout class partenariat

---
+++ GesMecenatBO/Partenariat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GesMecenatBO
+{
+    class Partenariat
+    {
+        private int id;
+        private float budget;
+        private float coutReel;
+
+        public Partenariat(int id, float budget, float coutReel)
+        {
+            this.id = id;
+            this.budget = budget;
+            this.coutReel = coutReel;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public float Budget { get => budget; set => budget = value; }
+        public float CoutReel { get => coutReel; set => coutReel = value; }
+    }
+}
