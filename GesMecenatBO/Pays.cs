b65ce9f092f47cde15eddb058b86da3c38f48bf1
ershadh60 <45233102+ershadh60@users.noreply.github.com>
2019-10-09 17:08:07 +0200

    jfj

---
+++ GesMecenatBO/Pays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GesMecenatBO
+{
+    public class Pays
+    {
+        private int id;
+        private string libelle;
+
+        public Pays(int id, string libelle)
+        {
+            this.Id = id;
+            this.Libelle = libelle;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Libelle { get => libelle; set => libelle = value; }
+    }
+}
