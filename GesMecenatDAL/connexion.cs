6632d186bfe87fedd25b9fa5137a5e9b24317d93
Lucas <45062643+LucasDavion@users.noreply.github.com>
2019-10-09 16:39:37 +0200

    .

---
+++ GesMecenatDAL/connexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GesMecenatDAL
+{
+    class connexion
+    {
+        static private SqlConnection objConnex;
+
+        static connexion()
+        {
+            objConnex = new SqlConnection();
+            objConnex.ConnectionString = ConfigurationManager.ConnectionStrings["AppGSB_PPE"].ConnectionString;
+        }
+
+        public static SqlConnection GetObjConnexion()
+        {
+            if (objConnex.State == System.Data.ConnectionState.Closed)
+            {
+                objConnex.Open();
+            }
+            return objConnex;
+        }
+
+        public static void CloseConnexion()
+        {
+            if (objConnex != null && objConnex.State != System.Data.ConnectionState.Closed)
+            {
+                objConnex.Close();
+            }
+
+        }
+    }
+}
