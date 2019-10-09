0dae9258981b7eb40899c39164c17df5fe822570
Lucas <45062643+LucasDavion@users.noreply.github.com>
2019-10-09 15:35:30 +0200

    Update Association.cs

--- GesMecenatBO/Association.cs
+++ GesMecenatBO/Association.cs
@@ -8,10 +8,19 @@ namespace GesMecenatBO
 {
     class Association
     {
+        private string nomAssociation;
+        private string mission;
+        private int id;
 
+        public Association(string nomAssociation, string mission, int id)
+        {
+            this.nomAssociation = nomAssociation;
+            this.mission = mission;
+            this.id = id;
+        }
 
-
-
-
+        public string NomAssociation { get => nomAssociation; set => nomAssociation = value; }
+        public string Mission { get => mission; set => mission = value; }
+        public int Id { get => id; set => id = value; }
     }
 }
