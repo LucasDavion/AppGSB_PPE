0c033d014aa28185ce29defa15ac817ae0f5e87c
dupontL64 <45233269+dupontL64@users.noreply.github.com>
2019-10-10 08:34:22 +0200

    maj class partenariat

--- GesMecenatBO/Partenariat.cs
+++ GesMecenatBO/Partenariat.cs
@@ -11,16 +11,19 @@ namespace GesMecenatBO
         private int id;
         private float budget;
         private float coutReel;
+        private Association uneAssociation;
 
-        public Partenariat(int id, float budget, float coutReel)
+        public Partenariat(int id, float budget, float coutReel, Association uneAssociation)
         {
             this.id = id;
             this.budget = budget;
             this.coutReel = coutReel;
+            this.uneAssociation = uneAssociation;
         }
 
         public int Id { get => id; set => id = value; }
         public float Budget { get => budget; set => budget = value; }
         public float CoutReel { get => coutReel; set => coutReel = value; }
+        public Association UneAssociation { get => uneAssociation; set => uneAssociation = value; }
     }
 }
