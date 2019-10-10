f76db44ef2a52e1ca845b02c56b50524a3644426
Lucas <45062643+LucasDavion@users.noreply.github.com>
2019-10-10 08:55:18 +0200

    Mission

--- GesMecenatDAL/AssociationDAO.cs
+++ GesMecenatDAL/AssociationDAO.cs
@@ -28,7 +28,7 @@ namespace GesMecenatDAL
 
             //On cree l'objet de type SqlCommand qui vas contenir la requete SQL permettant d'obtenir toutes les caracteristiques de tous les client 
             string sql;
-            sql = "select * from client join categorie on idCateg = categorie.id";
+            sql = "";
             SqlCommand maCommande = new SqlCommand(sql, cnx);
             maCommande.CommandText = sql;
 
