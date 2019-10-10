6339c065fcc087ee3a9a9a650e3db92a4c2a9516
dupontL64 <45233269+dupontL64@users.noreply.github.com>
2019-10-10 11:03:16 +0200

    maj PartenariatManager

--- GesMecenatBLL/PartenariatManager.cs
+++ GesMecenatBLL/PartenariatManager.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GesMecenatDAL;
+using GesMecenatBO;
 
 namespace GesMecenatBLL
 {
-    class PartenariatManager
+    public class PartenariatManager
     {
+        private static PartenariatManager uneInstance;
+
+        public static PartenariatManager GetInstance()
+        {
+            if (uneInstance == null)
+            {
+                uneInstance = new PartenariatManager();
+            }
+            return uneInstance;
+        }
+        private PartenariatManager()
+        {
+
+        }
+        public List<Partenariat> GetPartenariat()
+        {
+            return PartenariatDAO.GetInstance().GetPartenariat();
+        }
     }
 }
