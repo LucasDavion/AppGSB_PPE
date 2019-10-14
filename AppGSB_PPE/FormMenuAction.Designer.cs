bfa14df92f022ac0bac195b0921900e1e4c68499
Liankir <45237185+Liankir@users.noreply.github.com>
2019-10-14 13:36:27 +0200

    action

--- AppGSB_PPE/FormMenuAction.Designer.cs
+++ AppGSB_PPE/FormMenuAction.Designer.cs
@@ -34,9 +34,9 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.cbxCreation = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -82,7 +82,7 @@
             // 
             this.panel2.Controls.Add(this.btnModification);
             this.panel2.Controls.Add(this.btnSupression);
-            this.panel2.Location = new System.Drawing.Point(248, 93);
+            this.panel2.Location = new System.Drawing.Point(231, 93);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(210, 74);
             this.panel2.TabIndex = 4;
@@ -97,21 +97,6 @@
             this.panel3.Size = new System.Drawing.Size(213, 74);
             this.panel3.TabIndex = 5;
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 4);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(207, 21);
-            this.comboBox1.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(4, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
             // cbxCreation
             // 
             this.cbxCreation.AutoSize = true;
@@ -122,11 +107,26 @@
             this.cbxCreation.Text = "cree une action";
             this.cbxCreation.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(207, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
             // FormMenuAction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1435, 568);
+            this.ClientSize = new System.Drawing.Size(455, 180);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
