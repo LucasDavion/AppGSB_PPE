5100154bb60ba379a462b056a7488babe474881d
Lucas <45062643+LucasDavion@users.noreply.github.com>
2019-10-17 08:51:33 +0200

    .

--- AppGSB_PPE/FormConsultAssociationMission.Designer.cs
+++ AppGSB_PPE/FormConsultAssociationMission.Designer.cs
@@ -28,34 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dtgConsultAssociation = new System.Windows.Forms.DataGridView();
-            this.dtgMission = new System.Windows.Forms.DataGridView();
+            this.dtgConsult = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnMissions = new System.Windows.Forms.Button();
-            this.btnAssociation = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgConsultAssociation)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgMission)).BeginInit();
+            this.btnAssociation = new System.Windows.Forms.Button();
+            this.btnMissions = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgConsult)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dtgConsultAssociation
+            // dtgConsult
             // 
-            this.dtgConsultAssociation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgConsultAssociation.Location = new System.Drawing.Point(12, 157);
-            this.dtgConsultAssociation.Name = "dtgConsultAssociation";
-            this.dtgConsultAssociation.Size = new System.Drawing.Size(776, 281);
-            this.dtgConsultAssociation.TabIndex = 0;
-            this.dtgConsultAssociation.Visible = false;
-            // 
-            // dtgMission
-            // 
-            this.dtgMission.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgMission.Location = new System.Drawing.Point(12, 157);
-            this.dtgMission.Name = "dtgMission";
-            this.dtgMission.Size = new System.Drawing.Size(776, 281);
-            this.dtgMission.TabIndex = 1;
-            this.dtgMission.Visible = false;
+            this.dtgConsult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgConsult.Location = new System.Drawing.Point(12, 157);
+            this.dtgConsult.Name = "dtgConsult";
+            this.dtgConsult.Size = new System.Drawing.Size(776, 281);
+            this.dtgConsult.TabIndex = 0;
             // 
             // panel1
             // 
@@ -67,15 +55,14 @@
             this.panel1.Size = new System.Drawing.Size(776, 114);
             this.panel1.TabIndex = 2;
             // 
-            // btnMissions
+            // label1
             // 
-            this.btnMissions.Location = new System.Drawing.Point(262, 58);
-            this.btnMissions.Name = "btnMissions";
-            this.btnMissions.Size = new System.Drawing.Size(124, 42);
-            this.btnMissions.TabIndex = 0;
-            this.btnMissions.Text = "Missions";
-            this.btnMissions.UseVisualStyleBackColor = true;
-            this.btnMissions.Click += new System.EventHandler(this.btnMissions_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(319, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Affichage des données";
             // 
             // btnAssociation
             // 
@@ -87,14 +74,15 @@
             this.btnAssociation.UseVisualStyleBackColor = true;
             this.btnAssociation.Click += new System.EventHandler(this.btnAssociation_Click);
             // 
-            // label1
+            // btnMissions
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(319, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Affichage des données";
+            this.btnMissions.Location = new System.Drawing.Point(262, 58);
+            this.btnMissions.Name = "btnMissions";
+            this.btnMissions.Size = new System.Drawing.Size(124, 42);
+            this.btnMissions.TabIndex = 0;
+            this.btnMissions.Text = "Missions";
+            this.btnMissions.UseVisualStyleBackColor = true;
+            this.btnMissions.Click += new System.EventHandler(this.btnMissions_Click);
             // 
             // FormConsultAssociationMission
             // 
@@ -102,12 +90,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.dtgMission);
-            this.Controls.Add(this.dtgConsultAssociation);
+            this.Controls.Add(this.dtgConsult);
             this.Name = "FormConsultAssociationMission";
             this.Text = "FormConsultAssociation";
-            ((System.ComponentModel.ISupportInitialize)(this.dtgConsultAssociation)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgMission)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgConsult)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -116,8 +102,7 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dtgConsultAssociation;
-        private System.Windows.Forms.DataGridView dtgMission;
+        private System.Windows.Forms.DataGridView dtgConsult;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnAssociation;
--- GesMecenatDAL/MissionDAO.cs
+++ GesMecenatDAL/MissionDAO.cs
@@ -24,7 +24,6 @@ namespace GesMecenatDAL
 
         private MissionDAO()
         {
-
         }
 
         public List<Mission> GetMission()
